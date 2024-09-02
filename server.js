60d8df69aa6ff6e0031dd1e3b5727de36eb1b4c1
IRFAN ULLAH <faani094@gmail.com>
2024-09-02 16:50:42 +0500

    Update server.js
    Updated Server file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
+import path from 'path';
 
 // dotenv Configuration
 dotenv.config();
@@ -14,6 +15,16 @@ app.use(express.json())
 
 // Routes
 app.use('/api/v1/portfolio', require('./routes/portfolioRoutes'))
+
+// Static path 
+// changing to host the project
+app.use(express.static(path.join(__dirname , './client/build')))
+
+app.get('*' , function(req, res){
+    res.sendFile(path.join(__dirname , './client/build/index.html'));
+})
+
+
 
 
 // port
